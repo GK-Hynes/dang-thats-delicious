3dbcd03974561cc020b2dbbe329722b0534d88aa
Gerard Hynes <gkhynes@gmail.com>
2020-11-09 22:20:59 +0000

    Practice templating with pug

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-// Do work here
-router.get('/', (req, res) => {
-  res.send('Hey! It works!');
+router.get("/", (req, res) => {
+  res.render("hello", {
+    name: "Wes",
+    dog: req.query.dog
+  });
+});
+
+router.get("/reverse/:name", (req, res) => {
+  const reverse = [...req.params.name].reverse().join("");
+  res.send(reverse);
 });
 
 module.exports = router;
