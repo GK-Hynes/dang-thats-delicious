00d7a66b34aa5b4a9a01b1b521dee0eafd3e9a19
Gerard Hynes <gkhynes@gmail.com>
2020-11-15 20:52:46 +0000

    Aggregate stores by tag

--- models/Store.js
+++ models/Store.js
@@ -54,4 +54,22 @@ storeSchema.pre("save", async function (next) {
   next();
 });
 
+// Aggregate stores by tags
+storeSchema.statics.getTagsList = function () {
+  return this.aggregate([
+    { $unwind: "$tags" },
+    {
+      $group: {
+        _id: "$tags",
+        count: { $sum: 1 }
+      }
+    },
+    {
+      $sort: {
+        count: -1
+      }
+    }
+  ]);
+};
+
 module.exports = mongoose.model("Store", storeSchema);
