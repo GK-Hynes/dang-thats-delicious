801625bdd20bf46bc2506a403984edeba2202ff0
Gerard Hynes <gkhynes@gmail.com>
2020-11-17 22:17:36 +0000

    Add login and logout middleware

--- controllers/authController.js
+++ controllers/authController.js
@@ -6,3 +6,19 @@ exports.login = passport.authenticate("local", {
   successRedirect: "/",
   successFlash: "You are now logged in"
 });
+
+exports.logout = (req, res) => {
+  req.logout();
+  req.flash("success", "You are now logged out 👋");
+  res.redirect("/");
+};
+
+exports.isLoggedIn = (req, res, next) => {
+  // check if user is authenticated
+  if (req.isAuthenticated()) {
+    next(); // carry on, they're logged in
+    return;
+  }
+  req.flash("error", "Oops! You must be logged in");
+  res.redirect("/login");
+};
