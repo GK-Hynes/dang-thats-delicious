ebe62de0ff2b2a66291625c9f7529bf6c4f6b8d4
Gerard Hynes <gkhynes@gmail.com>
2020-11-23 21:54:16 +0000

    Add ability to heart stores

--- public/javascripts/modules/map.js
+++ public/javascripts/modules/map.js
@@ -7,51 +7,54 @@ const mapOptions = {
 };
 
 function loadPlaces(map, lat = 43.2, lng = -79.8) {
-  axios.get(`/api/stores/near?lat=${lat}&?lng=${lng}`).then((res) => {
-    const places = res.data;
-    if (!places.length) {
-      alert("No places found!");
-      return;
-    }
+  axios
+    .get(`/api/stores/near?lat=${lat}&?lng=${lng}`)
+    .then((res) => {
+      const places = res.data;
+      if (!places.length) {
+        alert("No places found!");
+        return;
+      }
 
-    const bounds = new google.maps.LatLngBounds();
+      const bounds = new google.maps.LatLngBounds();
 
-    const infoWindow = new google.maps.infoWindow();
+      const infoWindow = new google.maps.infoWindow();
 
-    const markers = places.map((place) => {
-      const [placeLng, placeLat] = place.location.coordinates;
-      const position = { lat: placeLat, lng: placeLng };
-      bounds.extend(position);
-      const marker = new google.maps.Marker({
-        map,
-        position
+      const markers = places.map((place) => {
+        const [placeLng, placeLat] = place.location.coordinates;
+        const position = { lat: placeLat, lng: placeLng };
+        bounds.extend(position);
+        const marker = new google.maps.Marker({
+          map,
+          position
+        });
+        marker.place = place;
+        return marker;
       });
-      marker.place = place;
-      return marker;
-    });
 
-    // Show details when marker clicked on
-    markers.forEach((marker) =>
-      marker.addListener("click", function () {
-        const html = `
+      // Show details when marker clicked on
+      markers.forEach((marker) =>
+        marker.addListener("click", function () {
+          const html = `
           <div className="popup">
           <a href="/store/${this.place.slug}">
           <img src="/uploads/${this.place.photo || "store.png"}" alt="${
-          this.place.name
-        }"/>
+            this.place.name
+          }"/>
           <p>${this.place.name} - ${this.place.location.address}</p>
             </a>  
           </div>
         `;
-        infoWindow.setContent(html);
-        infoWindow.open(map, this);
-      })
-    );
+          infoWindow.setContent(html);
+          infoWindow.open(map, this);
+        })
+      );
 
-    // Zoom map to fit all markers
-    map.setCenter(bounds.getCenter());
-    map.fitBounds(bounds);
-  });
+      // Zoom map to fit all markers
+      map.setCenter(bounds.getCenter());
+      map.fitBounds(bounds);
+    })
+    .catch(console.error);
 }
 
 function makeMap(mapDiv) {
